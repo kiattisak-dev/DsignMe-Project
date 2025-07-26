dd0095e93c7fc244fe654d790f0b09923305cfec
KiattisakDeeprasert <6531503199@lamduan.mfu.ac.th>
2025-07-27 03:20:31 +0700

    fix cors

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -1,24 +1,15 @@
 package middleware
 
 import (
-	"os"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func CorsMiddleware() fiber.Handler {
-	// ใช้ environment variable สำหรับ AllowOrigins (ถ้าไม่กำหนดใช้ค่า default)
-	allowedOrigins := os.Getenv("CORS_ALLOWED_ORIGINS")
-	if allowedOrigins == "" {
-		allowedOrigins = "https://dsignme-admin.vercel.app,https://www.dsignme.co"
-	}
-
 	return cors.New(cors.Config{
-		AllowOrigins:     allowedOrigins,
+		AllowOrigins:     "http://localhost:5173, http://localhost:3000, https://dsignme-admin.vercel.app, https://www.dsignme.co",
 		AllowMethods:     "GET,POST,PUT,PATCH,DELETE",
-		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
-		ExposeHeaders:    "Content-Length", // เพิ่มถ้าต้องการ expose header อื่น
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 	})
 }
