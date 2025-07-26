c284aebecafda2500719e743a2f3be2ef9b137cc
KiattisakDeeprasert <6531503199@lamduan.mfu.ac.th>
2025-07-27 00:23:19 +0700

    test logo page

--- user/src/main.tsx
+++ user/src/main.tsx
@@ -4,9 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
-const apiUrl = import.meta.env.VITE_API_URL;
-console.log('API URL:', apiUrl); // ทดสอบว่าได้ค่า
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
